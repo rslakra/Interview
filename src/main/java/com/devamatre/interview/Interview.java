47f880f6adf7b4954bb37baedfc22d037e3be667
Rohtash Lakra <rslakra@users.noreply.github.com>
2023-11-12 20:14:10 -0800

    added init files

---
+++ src/main/java/com/devamatre/interview/Interview.java
@@ -0,0 +1,23 @@
+package com.devamatre.interview;
+
+/**
+ * @author Rohtash Lakra
+ * @created 11/12/23 5:21 PM
+ */
+public class Interview {
+
+    /**
+     * @return
+     */
+    public int getYear() {
+        return 2023;
+    }
+
+    /**
+     * @return
+     */
+    public String getName() {
+        return "Rohtash";
+    }
+
+}
